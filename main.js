2883e08e1129ade5b3c1a3765d948a19211e9158
Aaravwhitehatjr <82259932+Aaravwhitehatjr@users.noreply.github.com>
2021-09-18 12:22:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log ("My First Console Project");
+
+var first_name= "Aarav";
+var last_name="Shrivastava";
+
+var full_name= first_name.concat(last_name);
+console.log(full_name);
+
+var a= 1;
+var b= 2;
+
+var add=1+2;
+console.log(add);
+
+var sub=1-2;
+console.log(sub);
+
+var multiply=1*2;
+console.log(multiply);
+
+var divide=1/2;
+console.log(divide);
+
+
